b99b402c02234c0b084504374038b161ceb22a59
webwerks1 <webwerks1@XYZ-123.local>
2019-02-21 20:19:33 +0530

    hooked droped api

--- src/ImagesContainer.js
+++ src/ImagesContainer.js
@@ -1,14 +1,27 @@
 import React from "react";
+import { DropTarget } from "react-dnd";
 import ImgDragSrc from "./imgDragSrc";
 
-const ImagesItems = props => {
-  const { imgsrc } = props;
+const spec = {
+  drop(props) {
+    console.log("droped", props);
+    return {};
+  }
+};
 
-  return (
+function collect(connect, monitor) {
+  return {
+    connectDropTarget: connect.dropTarget()
+  };
+}
+const ImagesItems = props => {
+  const { imgsrc, connectDropTarget } = props;
+
+  return connectDropTarget(
     <div style={{ borderStyle: "dashed", borderColor: "#948888" }}>
       <ImgDragSrc imgsrc={imgsrc} />
     </div>
   );
 };
 
-export default ImagesItems;
+export default DropTarget("item", spec, collect)(ImagesItems);
