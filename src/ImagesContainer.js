ed862dfdbda29c1e4bfca299c7626eb18f6d81af
webwerks1 <webwerks1@XYZ-123.local>
2019-02-21 17:42:06 +0530

    dnd added

--- src/ImagesContainer.js
+++ src/ImagesContainer.js
@@ -1,11 +1,28 @@
 import React from "react";
-
+import { DragSource } from "react-dnd";
+const itemSource = {
+  beginDrag(props) {
+    console.log("is dragingn");
+    return props.items;
+  },
+  endDrag(props, monitor, component) {
+    return props.handleProp(props.item.id);
+  }
+};
+function collect(connect, monitor) {
+  return {
+    connectDragSource: connect.dragSource(),
+    connectDragPreview: connect.dragPreview(),
+    isDragging: monitor.isDragging()
+  };
+}
 const ImagesItems = ({ imgsrc, ...props }) => {
-  return (
+  const { isDragging, connectDragSource, item } = props;
+  return connectDragSource(
     <div style={{ borderStyle: "dashed", borderColor: "#948888" }}>
       <img src={imgsrc} />
     </div>
   );
 };
 
-export default ImagesItems;
+export default DragSource("item", itemSource, collect)(ImagesItems);
